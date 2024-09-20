7980ff687c9ab8b6ee0b0562d354fcaa18a85b50
Rick Carstens <rick@ambergrouplimited.co.uk>
2024-09-20 11:18:03 +0100

    Initial mockup for CNC Marketplace

---
+++ script.js
@@ -0,0 +1,6 @@
+function simulateStaking() {
+    var stakeAmount = prompt("Enter the amount of ADA you want to stake:");
+    var reward = stakeAmount * 0.05;  // Assume a 5% reward for demo purposes
+    alert("You've earned " + reward + " CNC tokens for staking " + stakeAmount + " ADA.");
+  }
+  
